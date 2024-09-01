078290d49aaa66f14ef87d1253249301ebc77a82
Keenan Powell <kmodo1234@gmail.com>
2024-09-01 04:38:08 -0400

    adding new simulation game method to player class

--- player_class.py
+++ player_class.py
@@ -1,17 +1,18 @@
 import numpy as np
 
 class Player:
-    def __init__(self, skill: float, id: int):
+    def __init__(self, skill: float, id: int) -> None:
         self.skill = skill
         self.id = id
         self.wl_record = []
         self.game_record = []
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Player ID: {self.id},\t Player Skill: {self.skill}"
 
-    def play(self, player_2):
-        result = self.skill - player_2.skill + np.random.normal(0.0, (1 - max(self.skill, player_2.skill)) * 1) > 0
+    def play(self, player_2) -> None:
+        relative_log_difference = np.pow(10, (self.skill - player_2.skill)/0.2)
+        result = np.random.random() < relative_log_difference/(relative_log_difference + 1)
         self.wl_record.append(result)
         self.game_record.append(player_2.id)
         player_2.wl_record.append(not result)
